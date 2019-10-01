2a03b282eb4e139bbea07119514432f93fccf355
Daniel FISCHER <dfischer17@htl-grieskirchen.local>
2019-10-01 13:19:58 +0200

    example2 state end of lesson

---
+++ Beispiel2/src/net/htlgrieskirchen/pos3/streams/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgrieskirchen.pos3.streams;
+
+import java.util.Random;
+
+/**
+ *
+ * @author dfischer17
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        int[] arr = new Random().ints(10000, 0, 101).toArray();
+        
+        
+    }
+    
+    
+}
